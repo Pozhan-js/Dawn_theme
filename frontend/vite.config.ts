82c3119d90df5106f988e62adf92c2e6aa595cd6
hashMi <854059946@qq.com>
2026-06-02 17:57:05 +0800

    Add Vite and Shopify integration to frontend project
    - Introduced vite-plugin-shopify for streamlined asset management.
    - Updated Vite configuration to include Shopify plugin settings.
    - Created new entry point for Vue application and adjusted Liquid templates to utilize Vite's asset handling.
    - Added .shopifyignore to exclude unnecessary files from Shopify deployment.
    - Included new CSS and JS files for Vue components in the assets directory.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,25 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import shopify from 'vite-plugin-shopify'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    shopify({
+      themeRoot: '../',
+      sourceCodeDir: '.',
+      entrypointsDir: 'entrypoints',
+      snippetFile: 'vite-tag.liquid',
+    }),
     vue(),
     vueDevTools(),
   ],
   publicDir: false,
-  build: {
-    emptyOutDir: false,
-    outDir: '../assets',
-    rollupOptions: {
-      input: fileURLToPath(new URL('./src/main.ts', import.meta.url)),
-      output: {
-        entryFileNames: 'vue-nest-poc.js',
-        assetFileNames: 'vue-nest-poc.[ext]',
-      },
-    },
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
